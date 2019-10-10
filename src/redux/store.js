1031512c08c142ac1a83e2717f4046a70663635f
unknown <albanza38@gamil.com>
2019-10-10 21:58:01 +0200

    edit logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,9 +5,9 @@ import {persistStore} from 'redux-persist';
 
 const middlewares = [];
 
-// if(process.env.NODE_ENV === 'development'){
-//     middlewares.push(logger)
-// }
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger)
+}
 
  const store = createStore(rootReducer,applyMiddleware(...middlewares));
 
