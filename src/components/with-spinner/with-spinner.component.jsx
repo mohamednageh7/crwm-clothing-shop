329e4db52606299afe3805593771cd7c27f47bcf
unknown <albanza38@gamil.com>
2019-10-13 16:01:33 +0200

    update package

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
-import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+import {SpinnerContainer,SpinnerOverlay} from './with-spinner.styles';
 
-const WithSpinner = WrappedComponent => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
+const WithSpinner = WrappedComponent =>{
+
+const Spinner = ({isLoading, ...otherProps}) => {
     return isLoading ? (
-      <SpinnerOverlay>
-        <SpinnerContainer />
-      </SpinnerOverlay>
-    ) : (
-      <WrappedComponent {...otherProps} />
-    );
-  };
-  return Spinner;
+        <SpinnerOverlay>
+            <SpinnerContainer />
+        </SpinnerOverlay>
+    ): (
+        <WrappedComponent {...otherProps}/>
+    )
 };
+return Spinner
+}
 
 export default WithSpinner;
